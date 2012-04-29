187749a3195bedd40614714266506a9d2507c231
Todd Lunter <tlunter@gmail.com>
2012-04-28 23:18:39 -0400

    Add queueing, add new color to icons, add icon user, add deleting of items from the queue, add fade to popover

--- app/static/main.js
+++ app/static/main.js
@@ -6,95 +6,124 @@ jQuery.reduce = function(arr, valueInitial, fnReduce)
     return valueInitial;
 }
 
-jQuery(document).ajaxSend(function(event, xhr, settings) {
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    function sameOrigin(url) {
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-    function safeMethod(method) {
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-
-    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-        xhr.setRequestHeader("X-SRFToken", getCookie('csrftoken'));
-    }
-});
-
 $(document).ready(function() {
-
     var format_data = function(data) {
         var list = $.parseJSON(data);
-
+        
         var table = $.map(list, function(obj, n) {
-                var tr = $('<tr />');
-                var td_title = $('<td class="search-track-title" />');
-                var td_artist = $('<td class="search-track-artist" />');
-                var td_album = $('<td class="search-track-album" />');
-                var td_queue = $('<td class="search-track-queue" />');
-                td_title.html(obj.fields.title);
-                td_artist.html(obj.fields.artist);
-                td_album.html(obj.fields.album);
-                tr.append(td_title);
-                tr.append(td_artist);
-                tr.append(td_album);
-                tr.append(td_queue);
-                return tr
+            var tr = $('<tr />');
+            var td_title = $('<td />');
+            var td_artist = $('<td />');
+            var td_album = $('<td />');
+            var td_queue = $('<td />');
+            var button_queue = $('<button />');
+            var icon = $('<i />');
+            
+            icon.addClass('icon-plus-sign').addClass('icon-white');
+            
+            button_queue.addClass('queue-track-link').addClass('btn').addClass('btn-success');
+            button_queue.attr('name',obj.pk);
+            button_queue.append(icon);
+            
+            td_title.html(obj.fields.title).addClass('search-track-title');
+            td_artist.html(obj.fields.artist).addClass('search-track-artist');
+            td_album.html(obj.fields.album).addClass('search-track-album');
+            td_queue.append(button_queue).addClass('search-track-queue');
+            
+            tr.append(td_title);
+            tr.append(td_artist);
+            tr.append(td_album);
+            tr.append(td_queue);
+            
+            return tr
         });
 
         tbody = $.reduce(table, $('<tbody />'), function(total, i, item) { return total.append(item) });
         thead_search_results = $('<tr><th>Search Results:</th><th>' + list.length + ' Results</th></tr>');
         thead_titles = $('<tr><th>Title</th><th>Artist</th><th>Album</th><th>Queue?</th></tr>');
         thead = $('<thead />').append(thead_search_results).append(thead_titles);
-
+        
         return thead.add(tbody);
     }
-
+    
     $('.recent-track').popover({placement:'bottom'});
-
-    $('[name=search_term]').bindWithDelay("keyup",
-            function () {
-                $.ajax({
-                    url: '/instant-search/',
-                    type: 'POST',
-                    data: {'search_term': this.value},
-                    success: function(data) { $('[name=search_table]').html(format_data(data)) }
-                });
-            },
-            400);
-
-    /*$('.recent-track').bind('hover', function() {
-        $track = $(document).find('.popover')
+    
+    $('[name=search_term]').bindWithDelay("keyup",  function () {
         $.ajax({
-            url: 'http://api.wolframalpha.com/v2/query?appid=679H84-GQ872A7K5U&input=' + $track.find('.recent-track-album').text() + '-' + $track.find('.recent-track-album').text() + '&format=image',
-            success: function(data) {
-                xmlDoc = $.parseXML(data);
-                $xml = $( xmlDoc );
-                $image = $xml.find('pod[title="Album art"]').find('subpod').find('img');
-                $track.find('popover-content').add($image);
-            }
+            url: '/instant-search/',
+            type: 'POST',
+            data: {'search_term': this.value},
+            success: function(data) { $('[name=search_table]').html(format_data(data)) }
         });
-    });*/
+    },
+    400);
+    
+    $('#confirmModal').modal({
+        keyboard: true,
+        show: false
+    });
+    
+    $('#confirmModal').modal('hide');
+    
+    $('#successModal').modal({
+        keyboard: true,
+        show: false
+    });
+    
+    $('#successModal').modal('hide');
+});
+
+$(document).on('click','.queue-track-link', function(event) {
+    button_data = $(this);
+    $.ajax({
+        url: '/queue-track/',
+        type: 'POST',
+        data: {'track-pk': button_data.attr('name')},
+        success: function(data) { 
+            if(data == 'confirm') {
+                $('.queue-track-link-confirm').attr('name', button_data.attr('name'));
+                $('#confirmModal').modal('show');
+            }
+            else
+            {
+                $('#successModal').modal('show');
+                button_data.attr('disabled','disabled');
+            }
+        }
+    });
+});
+
+$(document).on('click', '.queue-track-link-confirm', function(event) {
+    button_data = $(this);
+    $('#confirmModal').modal('hide');
+    $.ajax({
+        url: '/queue-track/',
+        type: 'POST',
+        data: {'track-pk': button_data.attr('name'), 'confirm': true},
+        success: function(data) {
+            $('#successModal').modal('show');
+            $('.queue-track-link[name=' + button_data.attr('name') + ']').attr('disabled','disabled');
+        }
+    });
+});
+
+$(document).on('shown', '#successModal', function() {
+    window.setTimeout(function() {
+        $('#successModal').modal('hide');
+    }, 1000);
+});
+
+$(document).on('click', '.delete-queue-item', function(event) {
+    button_data = $(this);
+    $.ajax({
+        url: '/delete-queue-item/',
+        type: 'POST',
+        data: {'queue-item-pk': button_data.attr('name')},
+        success: function(data) {
+            var td = button_data.parent();
+            var tr = td.parent();
+            $(tr).popover('hide');
+            tr.remove();
+        }
+    });
 });
--- app/views.py
+++ app/views.py
@@ -22,9 +22,9 @@ import time
 import multiprocessing
 pool = multiprocessing.Pool()
 
-def index(request):
-    queue_items = QueueItem.objects.exclude(played = True, deleted = True).order_by('pk')
-
+def index_view(request):
+    queue_items = QueueItem.objects.filter(played = False, deleted = False).order_by('pk')[0:10]
+    
     return render_to_response('index.html',
             {'queue_items': queue_items},
             context_instance=RequestContext(request))
@@ -37,14 +37,14 @@ def logout_view(request):
 @login_required
 def profile_view(request, username = None):
     username = username if username else request.user.username
-
+    
     user_profile = get_object_or_404(User, username__exact = username)
     m = hashlib.md5()
     m.update(user_profile.email.lower())
     gravatar_email = m.hexdigest()
-
-    queue_tracks = QueueItem.objects.filter(user = request.user).exclude(deleted = True).order_by('-time_added')[:10]
-
+    
+    queue_tracks = QueueItem.objects.filter(user = user_profile).exclude(deleted = True).order_by('-time_added')[:10]
+    
     return render_to_response('profile.html',
             {'user_profile': user_profile,
              'gravatar_email': gravatar_email,
@@ -53,20 +53,16 @@ def profile_view(request, username = None):
 
 @login_required
 def search_view(request):
-
-    search_query = ''
     if request.method == "POST":
-        search_query = request.POST['search_term']
-
-        search_data = instant_search_view(request).content
-
-        search_data = simplejson.loads(search_data)
+        search_terms = request.POST['search_term']
+        
+        search_data = instant_search_fn(search_terms) 
         
         return render_to_response('search.html',
-                {'search_query': search_query,
+                {'search_terms': search_terms,
                  'search_data': search_data},
                 context_instance=RequestContext(request))
-
+    
     return render_to_response('search.html',
             context_instance=RequestContext(request))
 
@@ -75,90 +71,126 @@ def search_view(request):
 @csrf_exempt
 def instant_search_view(request):
     if request.method == 'POST' and len(request.POST['search_term']) > 1:
-        raw_search_terms = request.POST['search_term'].split()
-        search_terms = filter(None, raw_search_terms)
-        start = time.time()
+        search_terms = request.POST['search_term']
         
-        title_map       = map(lambda term: "(`title` LIKE '%%{0}%%') * {1}".format(term, len(term) * 3), search_terms)
-        album_map       = map(lambda term: "(`album` LIKE '%%{0}%%') * {1}".format(term, len(term)), search_terms)
-        artist_map      = map(lambda term: "(`artist` LIKE '%%{0}%%') * {1}".format(term, len(term) * 2), search_terms)
-        albumartist_map = map(lambda term: "(`albumartist` LIKE '%%{0}%%') * {1}".format(term, len(term) * 2), search_terms)
-
-        start = time.time()
-        query = Track.objects.extra(
-            select = {
-                'count': ' + '.join(title_map + album_map + artist_map + albumartist_map)
-            },
-            where = [' OR '.join(title_map + album_map + artist_map + albumartist_map)],
-            order_by = ['-count', 'artist'])
-
-        print "Query: ", query.query
-
-        query = list(query)
-        print "Processing: ", time.time() - start
-
-        return HttpResponse(serializers.serialize('json', query, fields = ('title','album','artist'))) #simplejson.dumps(tracks))
-
+        query = instant_search_fn(search_terms)
+        
+        return HttpResponse(serializers.serialize('json',
+            query,
+            fields = ('title','album','artist')))
+    
     raise Http404
 
-def queue_tracks_view(request):
+def instant_search_fn(search_term):
+    raw_search_terms = search_term.split()
+    search_terms = filter(None, raw_search_terms)
+
+    title_map       = map(lambda term: '(`title` LIKE %s) * {0}'.format(len(term) * 3), search_terms)
+    album_map       = map(lambda term: '(`album` LIKE %s) * {0}'.format(len(term)), search_terms)
+    artist_map      = map(lambda term: '(`artist` LIKE %s) * {0}'.format(len(term) * 2), search_terms)
+    albumartist_map = map(lambda term: '(`albumartist` LIKE %s) * {0}'.format(len(term) * 2), search_terms)
+
+    changed_terms = map(lambda term: '%{0}%'.format(term), search_terms)
+    
+    query = Track.objects.extra(
+        select = {
+            'count': ' + '.join(title_map + album_map + artist_map + albumartist_map)
+        },
+        select_params = changed_terms * 4,
+        where = [' OR '.join(title_map + album_map + artist_map + albumartist_map)],
+        params = changed_terms * 4,
+        order_by = ['-count', 'artist'])
+    
+    return query
+
+@login_required
+@csrf_exempt
+def queue_track_view(request):
     if request.method == 'POST':
-        if 'song' in request.POST:
-            return render_to_response('queue_tracks.html',
-                    {'song': request.POST['song']},
-                    context_instance=RequestContext(request))
-    raise Http404
+        if 'track-pk' in request.POST:
+            track_pk = request.POST['track-pk']
+            track = Track.objects.get(pk = track_pk)
+            
+            confirmation = True if 'confirm' in request.POST else False
+            
+            check_last_queue = QueueItem.objects.exclude(played = True, deleted = True).order_by('-pk')[0:2]
+            
+            queued = False
+            for queue_item in check_last_queue:
+                if queue_item.track.pk == track.pk:
+                    queued = True
+            
+            if queued and confirmation is False:
+                return HttpResponse('confirm')
+            
+            QueueItem.objects.create(user = request.user, track = track, played = False, deleted = False)
+            
+            return HttpResponse('success')
+        
+        raise Http404
+
+@login_required
+@csrf_exempt
+def delete_queue_item_view(request):
+    if request.method == 'POST':
+        queue_item_pk = request.POST['queue-item-pk']
+        queue_item = QueueItem.objects.get(pk = queue_item_pk)
+
+        if request.user == queue_item.user:
+            queue_item.deleted = True
+            queue_item.save()
+            return HttpResponse('success')
+
+    return Http404
 
 def load_tracks_view(request):
-
     if not request.user.has_perm('app.can_poll_for_tracks'):
         return render_to_response('load_tracks/permissions.html',
                 context_instance=RequestContext(request))
-
+    
     scans = request.user.scancount_set
     current_scans = scans.exclude(state = 'VE').order_by('-pk')
-
+    
     if current_scans.count() > 0:
         latest_scan = current_scans[0]
-
+        
         if latest_scan.state == 'FI':
             latest_scan.state = 'VE'
             latest_scan.save()
             return render_to_response('load_tracks/finished.html',
                     {'scan': latest_scan},
                     context_instance=RequestContext(request))
-
+        
         percentage = float(latest_scan.curr_count)/float(latest_scan.total_count)*100
         return render_to_response('load_tracks/running.html',
                 {'scan':latest_scan, 'percentage':percentage},
                 context_instance=RequestContext(request))
-
+    
     successful = start_track_update(request)
-
+    
     if not successful:
         return render_to_response('load_tracks/error.html',
                 {'error': 'no tracks'},
                 context_instance=RequestContext(request))
-
+    
     latest_scan = current_scans[0]
     return render_to_response('load_tracks/success.html',
             {'scan':latest_scan},
             context_instance=RequestContext(request))
 
 def start_track_update(request):
-
     tracks = subprocess.Popen(['find', MUSIC_FOLDER, '-iname', '*.mp3'], stdout=subprocess.PIPE)
     track_count = len(tracks.stdout.readlines())
-
+    
     if not track_count:
         return False
-
+    
     scanner = ScanCount(user = request.user,
                         curr_count = 0,
                         total_count = track_count,
                         state = 'ST')
     scanner.save()
-
+    
     def start_scan(scan_id): 
         ScanCount.objects.filter(pk = scan_id).update(state = 'SC')
         # Walk through the whole folder for just the files
@@ -168,7 +200,7 @@ def start_track_update(request):
             for mp3 in files:
                 # Only get the ID3 tags of mp3 files
                 if os.path.splitext(mp3)[1] == ".mp3":
-                
+                    
                     ScanCount.objects.filter(pk = scan_id).update(curr_count = F('curr_count') + 1)
                     audio = MP3(os.path.join(root,mp3), ID3=EasyID3)
 
@@ -191,19 +223,19 @@ def start_track_update(request):
                         title = unicode(raw_title, 'utf-8')
                     except KeyError:
                         title = u''
-
+                    
                     try:
                         raw_albumartist = (audio['performer'][0]).encode('utf-8')
                         albumartist = unicode(raw_albumartist, 'utf-8')
                     except KeyError:
                         albumartist = u''
-
+                    
                     raw_file_path = repr(os.path.join(root,mp3)).encode('utf-8')
                     file_path = unicode(raw_file_path, 'utf-8')
                     music.append({'artist':artist, 'album':album,
                                   'title':title, 'albumartist':albumartist,
                                   'file_path':file_path, 'length':length})
-                    
+            
             for track in music:
                 Track.objects.get_or_create(title = track['title'],
                                             artist = track['artist'],
@@ -211,9 +243,9 @@ def start_track_update(request):
                                             albumartist = track['albumartist'],
                                             file_path = track['file_path'],
                                             length = track['length'])
-
+        
         ScanCount.objects.filter(pk = scan_id).update(state = 'FI')
-
+    
     threading.Thread(target=start_scan, args=(scanner.pk,)).start()
     return True
 
